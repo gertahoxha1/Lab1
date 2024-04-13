35d6f436aa9c989b9437a8c980e893b7a1710d0b
Olsahalilaj <oh68588@ubt-uni.net>
2024-04-14 01:19:54 +0200

    About Us Done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Register from "./components/Register";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import Services from "./components/Services";
 function App() {
   
   return (
@@ -8,8 +9,10 @@ function App() {
     <Navbar/>
     <Home/>
     <Register/>
+    <Services/>
     </>
   ); 
 }
 
 export default App;
+
--- src/data.js
+++ src/data.js
@@ -5,4 +5,23 @@ export const pageLinks = [
     { id: 3, href: "#about", text: "about" },
     { id: 4, href: "#payment", text: "payment" },
   ];
-  
+  export const services = [
+    {
+      id: 1,
+      icon: "fas fa-sitemap fa-fw",
+      title: "Organization",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.",
+    },
+    {
+      id: 2,
+      icon: "fas fa-database fa-fw",
+      title: "Usage",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.",
+    },
+    {
+      id: 3,
+      icon: "fas fa-file fa-fw",
+      title: "Reference",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.",
+    },
+  ];
